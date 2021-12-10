7312ad2764c18fa2bb0910c42707aa140126b351
Chelsea Lipson <chelseamlipson@gmail.com>
2021-12-09 21:03:39 -0800

    Did basic webpack setup.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return <h1>Test</h1>
+  }
+}
+
+export default App;
