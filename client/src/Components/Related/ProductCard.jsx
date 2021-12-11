4135c8c67ce9cabbffcc4ee4a240fc81151cdf99
Sebastian <sebastianfox81@gmail.com>
2021-12-11 16:10:41 -0500

    Changed file name

---
+++ client/src/Components/Related/ProductCard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class ProductCard extends Component {
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
