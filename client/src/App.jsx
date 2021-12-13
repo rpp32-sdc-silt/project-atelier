c5bd785af5e3052049b85884929f0a7c899bd012
Chelsea Lipson <chelseamlipson@gmail.com>
2021-12-13 11:58:08 -0800

    Able to retrieve data from the API.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Overview/>
+        <Overview token={token}/>
       </div>
     )
   }
--- client/src/Components/Overview/Overview.jsx
+++ client/src/Components/Overview/Overview.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      products: []
+    }
+    this.apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp'
+  }
+
+  componentDidMount() {
+    axios.get(this.apiUrl + '/products', {headers: {"Authorization": this.props.token}})
+    .then((results) => {
+      this.setState({products: results.data})
+    })
   }
 
   render() {
     return (
       <div>
         <h2>Carousel</h2>
+        <div>{this.state.products.map(product => {
+          return <div key={product.id}>{product.name}</div>
+        })}</div>
         <h3>Style selector</h3>
         <h3>Cart</h3>
         <h3>Description</h3>
