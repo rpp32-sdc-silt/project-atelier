2746c2cdc3075c26dd84521586d2a3eab74c9b13
mendelorian <ipanda303@gmail.com>
2021-12-13 20:20:12 -0800

    Added basic React setup

---
+++ client/src/Components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RR extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Ratings and Reviews Component goes here</div>
+    )
+  }
+}
+
+export default RR;
