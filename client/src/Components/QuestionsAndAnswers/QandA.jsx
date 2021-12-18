750cc45bf0d95caf256e75749b37b4570d428921
Dakota <lustdakota14@gmail.com>
2021-12-18 09:48:28 -0700

    Skeleton of Question and AnswerList subcomponents complete

--- client/src/Components/QuestionsAndAnswers/QandA.jsx
+++ client/src/Components/QuestionsAndAnswers/QandA.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Question from './Question.jsx';
 
 class QandA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      questions: []
     }
+  }
+
+  //method to retrieve data from api on render
+  componentDidMount() {
+    //make call to api
+    //store questions data in state
   }
 
   render() {
     <div id="QandA">
-
+      {
+        this.state.questions.map((question) => {
+          return <Question question={question} />
+        });
+      }
     </div>
   }
 }
