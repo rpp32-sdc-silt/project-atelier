3d4123fde88aff8940d2a190b99dbeea025592b7
Sebastian <sebastianfox81@gmail.com>
2021-12-18 15:40:29 -0500

    Cleaned up Related Component for pull request

--- client/src/Components/Related/Related.jsx
+++ client/src/Components/Related/Related.jsx
@@ -3,8 +3,6 @@ import ProductList from './ProductList.jsx';
 import axios from 'axios'
 import token from '../../../../config.js';
 
-
-
 export default class Related extends Component {
   constructor(props) {
     super(props)
@@ -25,7 +23,6 @@ export default class Related extends Component {
   componentDidMount() {
     this.getAllRelatedProductIds();
     this.getAllProductsInfoFromId();
-    // this.getOneProductById();
   }
 
   getAllRelatedProductIds() {
@@ -36,27 +33,7 @@ export default class Related extends Component {
 
   getAllProductsInfoFromId() {
     let counter = this.state.endPointList.flat();
-    console.log(counter)
   }
-//   const arr = ['one', 'two', 'three', 'four'];
-
-// function func(arr) {
-//   let count = arr.length;
-//   while(count > 0) {
-//     console.log(arr[0])
-//     arr = arr.slice(1)
-//     count--
-//   }
-//   return;
-// }
-
-// getOneProductById() {
-  //   axios.defaults.headers.common['Authorization'] = token
-  //   axios.get(this.props.apiUrl + '/products/59554')
-  //   .then((results) => { this.setState({product: results.data})});
-  // }
-
-
 
   endPoints() {
     const list = []
@@ -65,17 +42,12 @@ export default class Related extends Component {
       let id = this.state.relatedProductsIdList[i];
       list.push(this.props.apiUrl + '/products/' + id)
     }
-    // console.log(this.state.relatedProductsIdList.length)
     this.state.endPointList.push(list)
 
   }
 
   render() {
     this.endPoints();
-
-    console.log(this.state.endPointList.flat().map(item => {
-      return item
-    }))
     return (
       <div>
         {/* <ProductList products={this.state.products}/> */}
