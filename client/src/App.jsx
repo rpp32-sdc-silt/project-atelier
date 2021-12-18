223f665e3c733e7a7c3e1d2fd6906c183d328973
Sebastian <sebastianfox81@gmail.com>
2021-12-18 15:35:07 -0500

    Imported Related component to main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Overview from './Components/Overview/Overview.jsx';
+import Related from './Components/Related/Related.jsx'
 import RR from './Components/RatingsAndReviews/RatingsAndReviews.jsx';
 import token from '../../config.js';
 class App extends React.Component {
@@ -17,6 +18,7 @@ class App extends React.Component {
         <RR apiUrl={this.apiUrl} token={token} currentProduct={this.state.currentProduct} />
         {/*Related
         Q&A*/}
+        <Related apiUrl={this.apiUrl}/>
       </div>
     )
   }
