c96e7da717db2808f2bb17bc68097145446600e1
Sebastian <sebastianfox81@gmail.com>
2021-12-20 16:04:52 -0500

    Pass token prop into Related component

--- client/src/Components/Related/Related.jsx
+++ client/src/Components/Related/Related.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import ProductList from './ProductList.jsx';
 import axios from 'axios'
-import token from '../../../../config.js';
 
 export default class Related extends Component {
   constructor(props) {
     super(props)
 
     this.getAllRelatedProductIds = this.getAllRelatedProductIds.bind(this);
-    // this.getOneProductById = this.getOneProductById.bind(this);
-    this.endPoints = this.endPoints.bind(this);
-    this.getAllProductsInfoFromId = this.getAllProductsInfoFromId.bind(this)
+    this.getAllEndPoints = this.getAllEndPoints.bind(this);
 
     this.state = {
       relatedProductsIdList: [],
-      product: [],
       endPointList: []
 
     }
@@ -22,32 +18,27 @@ export default class Related extends Component {
 
   componentDidMount() {
     this.getAllRelatedProductIds();
-    this.getAllProductsInfoFromId();
+    this.getAllEndPoints();
   }
 
   getAllRelatedProductIds() {
-    axios.defaults.headers.common['Authorization'] = token
+    axios.defaults.headers.common['Authorization'] = this.props.token
     axios.get(this.props.apiUrl + '/products/59553/related')
-    .then((results) => { this.setState({relatedProductsIdList: results.data})});
+    .then((results) => { this.setState({relatedProductsIdList: results.data})})
   }
 
-  getAllProductsInfoFromId() {
-    let counter = this.state.endPointList.flat();
-  }
-
-  endPoints() {
-    const list = []
-    for (var i = 0; i < this.state.relatedProductsIdList.length; i++) {
-
+  getAllEndPoints() {
+    let list = [];
+    for (let i = 0; i < this.state.relatedProductsIdList.length; i++) {
       let id = this.state.relatedProductsIdList[i];
-      list.push(this.props.apiUrl + '/products/' + id)
+        list.push(this.props.apiUrl + '/products/' + id)
     }
-    this.state.endPointList.push(list)
-
+    return list
   }
 
   render() {
-    this.endPoints();
+    // console.log(this.getAllEndPoints());
+    // console.log(this.state.endPointList)
     return (
       <div>
         {/* <ProductList products={this.state.products}/> */}
