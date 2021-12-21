2c3bf8ed29c126fda2425752e960cc4cb9fdc319
Sebastian <sebastianfox81@gmail.com>
2021-12-20 22:21:23 -0500

    Added css to ProductList component

--- client/src/Components/Related/ProductCard.jsx
+++ client/src/Components/Related/ProductCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ProductCard({ key, id, image, category, name, price}) {
  // console.log(category)
  return (
-   <div key={id}>
+   <div className="card" key={id}>
        <h3>{image}</h3>
        <h3>{category}</h3>
        <h3>{name}</h3>
