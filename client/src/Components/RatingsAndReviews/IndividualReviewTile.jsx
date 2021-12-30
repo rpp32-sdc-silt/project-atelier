d3eba168100b9a44f2031ac30be32a4100bfb25a
mendelorian <ipanda303@gmail.com>
2021-12-29 19:05:46 -0800

    Added stars with fixed rating

--- client/src/Components/RatingsAndReviews/IndividualReviewTile.jsx
+++ client/src/Components/RatingsAndReviews/IndividualReviewTile.jsx
@@ -8,11 +8,24 @@ class IndividualReviewTile extends React.Component {
 
   render() {
     const {review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness, photos} = this.props.review;
+    var count = 0;
+    for (var i = 0; i < 5; i++) {
+      if (count < rating) {
+
+        count++;
+      }
+    }
     return (
       <div>
         <b>IndividualReviewTile</b>
         <div className="rr-top-bar">
-          <span className="rating">Rating: {rating}</span>
+          <span className="rating">Rating: {rating}
+            <span className="fa fa-star rr-checked"></span>
+            <span className="fa fa-star rr-checked"></span>
+            <span className="fa fa-star rr-checked"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+          </span>
           <span className="recommend">{recommend ? 'Recommended' : null}</span>
           <span className="reviewer_name">Reviewer: {reviewer_name}</span>
           <span className="date">Date: {date}</span>
