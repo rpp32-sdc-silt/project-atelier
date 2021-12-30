f052486249ac6ea5c721a769ddfaa1844bb5c04b
Dakota <lustdakota14@gmail.com>
2021-12-29 18:51:55 -0700

    Rendered question body, working on answer list presentation

--- client/src/Components/QuestionsAndAnswers/AnswersList.jsx
+++ client/src/Components/QuestionsAndAnswers/AnswersList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class AnswersList extends React.Component {
   constructor(props) {
@@ -12,17 +13,30 @@ class AnswersList extends React.Component {
   //method to get answers list for particular questions id passed through props
   componentDidMount() {
     //make call to api for list using question id
-    //save answers list to state
+    axios.get(this.props.apiUrl + '/qa/questions/' + this.props.id + '/answers', {
+      headers: {
+        'Authorization': this.props.token
+      },
+      params: {
+        question_id: this.props.id
+      }
+    }).then((results) => {
+      //save answers list to state
+      this.setState({answers: results.data.results});
+    }).catch((err) => {console.log('Error getting answers from API: ' + err)});
   }
 
   render() {
     return (
       <div>
-        {/* Call getList here */}
+        {console.log(this.state.answers)}
+        <h2>A: </h2>
         <ol>
           {
             this.state.answers.map((answer, index) => {
-              return <li key={index}>{/*Answer information*/}</li>
+              return <li key={index}>
+                <p>{answer.body}</p>
+              </li>
             })
           }
         </ol>
