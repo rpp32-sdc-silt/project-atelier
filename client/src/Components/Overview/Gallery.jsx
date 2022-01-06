4274014c2f4489984f3fe6accd8a2cb4b101ec5e
Chelsea Lipson <chelseamlipson@gmail.com>
2022-01-05 19:26:42 -0800

    Added space.

--- client/src/Components/Overview/Gallery.jsx
+++ client/src/Components/Overview/Gallery.jsx
@@ -13,7 +13,7 @@ class Gallery extends React.Component {
       <>
         <img className="ov-gallery" src={this.props.prevPhoto}></img>
         <button id="back" className='ov-changePhoto' onClick={this.props.changePhoto}>←</button>
-        <img className="ov-gallery" alt={this.props.productInfo.description}src={this.props.photo} onClick={this.props.toggleModal}></img>
+        <img className="ov-gallery" alt={this.props.productInfo.description} src={this.props.photo} onClick={this.props.toggleModal}></img>
         <button id="forward" className='ov-changePhoto' onClick={this.props.changePhoto}>→</button>
         <img className="ov-gallery" src={this.props.nextPhoto}></img>
       </>
