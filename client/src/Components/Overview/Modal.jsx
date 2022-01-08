12221279d9b825017675a73b35181ea77be76f81
Chelsea Lipson <chelseamlipson@gmail.com>
2022-01-08 12:27:40 -0800

    Added image pan to modal.

--- client/src/Components/Overview/Modal.jsx
+++ client/src/Components/Overview/Modal.jsx
@@ -6,34 +6,42 @@ class Modal extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      zoom: false
+      zoom: false,
+      screenWidth: null,
+      screenHeight: null
     };
     this.changeZoomLevel = this.changeZoomLevel.bind(this);
+    this.panOnMouseMove = this.panOnMouseMove.bind(this);
   }
 
-  changeZoomLevel (e) {
+  changeZoomLevel(e) {
     //original state: right -100px
     //zoomed state: right: 225px;
     if (this.state.zoom === false) {
-      this.setState({zoom: true});
+      this.setState({ zoom: true });
+      this.setState({ screenWidth: e.view.screen.width, screenHeight: e.view.screen.height });
       $(e.target).addClass('ov-zoomedIn').removeClass('ov-zoomedOut');
-      $('.ov-modal .ov-thumbnail').css('right', 225);
     } else {
-      this.setState({zoom: false});
+      this.setState({ zoom: false });
       $(e.target).addClass('ov-zoomedOut').removeClass('ov-zoomedIn');
-      $('.ov-modal .ov-thumbnail').css('right', -100);
     }
   }
 
-  render(){
+  panOnMouseMove(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    $(e.target).css({'transform-origin': `${x - (this.state.screenWidth * 0.5)}px ${y}px`});
+  }
+
+  render() {
     return (
       <div className="ov-modal">
         <div className='ov-thumbnail'>
           {this.props.photos.map((photo, index) => {
             return <img className='ov-thumbnail-photo' id={index} src={photo.thumbnail_url} onClick={this.props.changePhoto}></img>
           })}</div>
-          <img className='ov-zoomedOut ov-modal-image' src={this.props.photo} alt={this.props.productInfo.description} onClick={this.changeZoomLevel}></img>
-          <button id="ov-toggleModal" onClick={this.props.toggleModal}>Close</button>
+        <img className='ov-zoomedOut ov-modal-image' src={this.props.photo} alt={this.props.productInfo.description} onClick={this.changeZoomLevel} onMouseMove={this.panOnMouseMove} ></img>
+        <button id="ov-toggleModal" onClick={this.props.toggleModal}>Close</button>
       </div>
     )
   }
