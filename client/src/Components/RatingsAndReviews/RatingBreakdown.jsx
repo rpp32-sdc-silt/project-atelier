78f39fad77a643ddc68e3ee4fff6a41cb7c47a9a
mendelorian <ipanda303@gmail.com>
2022-01-11 00:59:08 -0800

    Using meta data instead of limited reviews

--- client/src/Components/RatingsAndReviews/RatingBreakdown.jsx
+++ client/src/Components/RatingsAndReviews/RatingBreakdown.jsx
@@ -3,44 +3,41 @@ import React from 'react';
 class RatingBreakdown extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+    };
   }
 
-  calculateAvg(reviews) {
-    var avg = 0;
-    reviews.forEach((review) => {
-      avg += review.rating;
-    })
-    avg /= reviews.length;
+  calculateAvg(ratings) {
+    // reviews.forEach((review) => {
+    //   avg += review.rating;
+    // })
+    let num = parseInt(ratings['1']) + parseInt(ratings['2']) + parseInt(ratings['3']) + parseInt(ratings['4']) + parseInt(ratings['5']);
+    let avg = parseInt(ratings['1']) + (parseInt(ratings['2']) * 2) + (parseInt(ratings['3']) * 3) + (parseInt(ratings['4']) * 4) + (parseInt(ratings['5']) * 5);
+    avg /= num;
+    avg = avg.toFixed(2);
     return avg;
   }
 
-  calculateRecommended(reviews) {
-    var percent = 0;
-    reviews.forEach((review) => {
-      if (review.recommend) {
-        percent++;
-      }
-    })
-    percent = (percent / reviews.length) * 100;
-    return `${percent}%`;
-    // ============================================================
-    // Should recommendation % come from review data or meta data?
-    // ============================================================
-    // console.log('meta', meta);
-    // if (Object.keys(meta).length > 0) {
-    //   var percent = meta.recommended.true / (meta.recommended.true + meta.recommended.false) * 100;
-    //   return `${percent}%`;
-    // }
+  calculateRecommended(meta) {
+    if (Object.keys(meta).length > 0) {
+      var percent = parseInt(meta.recommended.true) / (parseInt(meta.recommended.true) + parseInt(meta.recommended.false)) * 100;
+      percent = percent.toFixed(2);
+      return `${percent}%`;
+    }
   }
 
   render() {
-    // console.log(this.props.meta);
     var metaData = this.props.meta;
     var display;
     if (Object.keys(metaData).length > 0) {
+      let totalReviews = parseInt(this.props.meta.recommended.false) + parseInt(this.props.meta.recommended.true);
       display =
         <div>
+          <b>RatingBreakdown</b>
+          <div>Number of reviews: {totalReviews}</div>
+          <div>Average Rating: {this.calculateAvg(metaData.ratings)}</div>
+          <div>Recommended: {this.calculateRecommended(metaData)}</div>
           <div>5 stars: {this.props.meta.ratings[5]}</div>
           <div>4 stars: {this.props.meta.ratings[4]}</div>
           <div>3 stars: {this.props.meta.ratings[3]}</div>
@@ -53,12 +50,7 @@ class RatingBreakdown extends React.Component {
 
     return (
       <div>
-        <b>RatingBreakdown</b>
-        <div>Number of reviews: {this.props.reviews.length}</div>
-        <div>Average Rating: {this.calculateAvg(this.props.reviews)}</div>
-        <div>Recommended: {this.calculateRecommended(this.props.reviews)}</div>
         {display}
-        <br/>
       </div>
     )
   }
