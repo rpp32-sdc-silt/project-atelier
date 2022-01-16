831d70970bcd22d5a15789017ba9e6dcea1f55e7
Dakota <lustdakota14@gmail.com>
2022-01-16 03:01:55 -0700

    Yes link click increases helpfulness by 1, once only

--- client/src/Components/QuestionsAndAnswers/AnswersList.jsx
+++ client/src/Components/QuestionsAndAnswers/AnswersList.jsx
@@ -76,7 +76,10 @@ class AnswersList extends React.Component {
                   <p className='timeAnswered'>{monthName + ' ' + day + ', ' + year}</p>
                   <p> | </p>
                   <p className='QandAHelpfulAnswer'>Helpful? </p>
-                  <a className='QandAyes' onClick={(event) => { this.props.trackClicks(event, 'Questions & Answers'); }}>Yes ({answer.helpfulness})</a>
+                  <p className='QandAyes' onClick={(event) => {
+                    this.props.trackClicks(event, 'Questions & Answers');
+                    event.target.textContent = `Yes (${answer.helpfulness + 1})`;
+                  }}>Yes ({answer.helpfulness})</p>
                   <p> | </p>
                   <p className='QandAreportAnswer'>Report</p>
                 </li>
@@ -87,7 +90,10 @@ class AnswersList extends React.Component {
                   <p className='timeAnswered'>{monthName + ' ' + day + ', ' + year}</p>
                   <p> | </p>
                   <p className='QandAHelpfulAnswer'>Helpful? </p>
-                  <a className='QandAyes' onClick={(event) => { this.props.trackClicks(event, 'Questions & Answers'); }}>Yes ({answer.helpfulness})</a>
+                  <a className='QandAyes' onClick={(event) => {
+                    this.props.trackClicks(event, 'Questions & Answers');
+                    event.target.textContent = `Yes (${answer.helpfulness + 1})`;
+                  }}>Yes ({answer.helpfulness})</a>
                   <p> | </p>
                   <p className='QandAreportAnswer'>Report</p>
                 </li>
