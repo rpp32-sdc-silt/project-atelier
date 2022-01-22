63c10da209edf51769baa8bd94a57642aab888e3
Chelsea Lipson <chelseamlipson@gmail.com>
2022-01-21 18:32:27 -0800

    Fixing styles bug

--- client/src/Components/Overview/Overview.jsx
+++ client/src/Components/Overview/Overview.jsx
@@ -95,15 +95,18 @@ class Overview extends React.Component {
   }
 
   changeStyle(event) {
+    var prevPhotoId = this.state.prevPhotoId;
+    var currentPhotoId = this.state.currentPhotoId
+    var nextPhotoId = this.state.nextPhotoId;
     var checkmark = '<span class="fa fa-check ov-checkmark"></span>';
     var id = Number.parseInt(event.target.id);
     //add checkmark to current style
     $('.fa-check').remove();
     $('#' + event.target.id).after(checkmark);
     this.setState({ currentStyle: id })
-    this.setState({ prevPhotoUrl: this.state.styles[id].photos[this.state.prevPhotoId].url })
-    this.setState({ currentPhotoUrl: this.state.styles[id].photos[this.state.currentPhotoId].url })
-    this.setState({ nextPhotoUrl: this.state.styles[id].photos[this.state.nextPhotoId].url })
+    this.setState({ prevPhotoUrl: this.state.styles[id].photos[prevPhotoId].url})
+    this.setState({ currentPhotoUrl: this.state.styles[id].photos[currentPhotoId].url })
+    this.setState({ nextPhotoUrl: this.state.styles[id].photos[nextPhotoId].url})
     this.setState({ inventory: this.state.styles[id].skus })
   }
 
