fd88d6fbcaae2d525940f9fae02f53f9abd2239c
mendelorian <ipanda303@gmail.com>
2022-02-24 19:43:30 -0800

    Fixed rating breakdown. Converted routing to server instead of direct for /product endpoints.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentProduct: 59553
+      // currentProduct: 64620
+      currentProduct: 1
     }
-    this.apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
+    // this.apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
+    this.apiUrl = 'http://localhost:3000';
     this.trackClicks = this.trackClicks.bind(this);
   }
 
